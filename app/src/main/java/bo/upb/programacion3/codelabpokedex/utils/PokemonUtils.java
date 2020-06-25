34978beb106c6e2502d28a91b42a5175e5835f5a
LaBalaElmo <jrttaquichiri@gmail.com>
2020-06-24 23:01:13 -0400

    Tarea

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -27,6 +27,16 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(13, "Mewtwo", R.drawable.mewtwo, "Psycho"));
+        pokemons.add(new Pokemon(14, "Cubone", R.drawable.cubone, "Ground"));
+        pokemons.add(new Pokemon(15, "Audino", R.drawable.audino, "Normal"));
+        pokemons.add(new Pokemon(16, "Chansey", R.drawable.chansey, "Normal"));
+        pokemons.add(new Pokemon(17, "Chespin", R.drawable.chespin, "Plant"));
+        pokemons.add(new Pokemon(18, "Chimchar", R.drawable.chimchar, "Fire"));
+        pokemons.add(new Pokemon(19, "Clefable", R.drawable.clefable, "Fairy"));
+        pokemons.add(new Pokemon(20, "Cyndaquil", R.drawable.cyndaquil, "Fire"));
+        pokemons.add(new Pokemon(21, "Dedenne", R.drawable.dedenne, "Electric"));
+        pokemons.add(new Pokemon(22, "Diglett", R.drawable.diglett, "Ground"));
         return pokemons;
     }
 }
